68ecdedbf4c7d5081f0a62bd3984ffedc7e9b021
Himani Ahuja <himani.ahuja@sv.cmu.edu>
2011-09-26 20:14:47 -0700

    Starting a Twitter

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,25 @@
+class PostsController < ApplicationController
+
+  def index
+    @posts = Post.all
+    respond_to do |format|
+      format.html
+    end
+  end
+
+  def create
+    @post = Post.create(:message => params[:message])
+    respond_to do |format|
+       if @post.save
+         format.html{ redirect_to posts_path}
+         format.js
+       else
+         flash[:notice] = "Message failed to save"
+         format.html {redirect_to posts_path}
+       end
+    end
+
+  end
+
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twitter::Application.config.secret_token = 'c58ccc6fdc446a88cb10f7ea0c04a801f64b965014e8683315dfa5a4062858c28deb34d6d72c949b78a354df00a11b2f244926419019f3d67e827bb0b77da96d'
